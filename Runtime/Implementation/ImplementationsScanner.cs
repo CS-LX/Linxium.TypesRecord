49005809c82499656d7b71fedc595cb162620255
LX.. <1659404657@qq.com>
2025-12-13 22:24:52 +0800

    make scanner with generic abstract

--- Runtime/Implementation/ImplementationsScanner.cs
+++ Runtime/Implementation/ImplementationsScanner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace TypesRecord.Implementation {
-    public class ImplementationsScanner<T> : ITypesScanner {
+    public abstract class ImplementationsScanner<T> : ITypesScanner {
         public string ID => nameof(T);
         public Type[] ScanTypes(List<Type> allTypes) => allTypes.Where(x => typeof(T).IsAssignableFrom(x) && !x.IsAbstract).ToArray();
     }
